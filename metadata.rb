4222b25d38357066fcb178179a6cdb7775a05d56
Michael Burns <michael.burns@rackspace.com>
2015-05-30 22:02:18 -0700

    update metadata with new maintainer and CI links

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
 name             "libyaml"
-maintainer       "Cassiano Leal"
-maintainer_email "cl@cassianoleal.com"
+maintainer       "mburns"
+maintainer_email "michael@mirwin.net"
 license          "Apache 2.0"
 description      "Installs/Configures libyaml"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 recipe           "libyaml", "Installs libyaml from source"
+source_url       "https://github.com/mburns/cookbook-libyaml" if respond_to?(:source_url)
+issues_url       "https://github.com/mburns/cookbook-libyaml/issues" if respond_to?(:issues_url)
 
 %w{ debian ubuntu }.each do |os|
   supports os
