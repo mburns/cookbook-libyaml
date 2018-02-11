8890b6f862bc6552e17726534bc7ff4d8a428b60
Michael Burns <michael@mirwin.net>
2018-02-11 03:31:44 -0800

    updates

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michael@mirwin.net'
 license 'Apache 2.0'
 description 'Installs/Configures libyaml'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 recipe 'libyaml', 'Installs libyaml from source'
 source_url 'https://github.com/mburns/cookbook-libyaml' if respond_to?(:source_url)
 issues_url 'https://github.com/mburns/cookbook-libyaml/issues' if respond_to?(:issues_url)
